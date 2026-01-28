11bf8aff6e8c33e12fc99bdc26bc8adc668a39ee
Aaron Syrnikov <114866139+syrnikov@users.noreply.github.com>
2026-01-28 13:59:10 +0200

    Randomize pebble variants and load data pack

--- src/main/java/com/noxtrnal/vestiges/block/PebbleBlock.java
+++ src/main/java/com/noxtrnal/vestiges/block/PebbleBlock.java
@@ -4,20 +4,26 @@ import com.noxtrnal.vestiges.registry.VestigesBlockTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.EnumProperty;
+import net.minecraft.util.StringIdentifiable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 import net.minecraft.world.WorldView;
-import net.minecraft.world.WorldAccess;
 import net.minecraft.world.tick.ScheduledTickView;
+import org.jetbrains.annotations.Nullable;
 
 public class PebbleBlock extends Block {
     private static final VoxelShape SHAPE = Block.createCuboidShape(3.0, 0.0, 3.0, 13.0, 2.0, 13.0);
+    public static final EnumProperty<PebbleVariant> VARIANT = EnumProperty.of("variant", PebbleVariant.class);
 
     public PebbleBlock(Settings settings) {
         super(settings);
+        setDefaultState(getStateManager().getDefaultState().with(VARIANT, PebbleVariant.SMALL));
     }
 
     // UPDATED: world is now WorldView in 1.21.x
@@ -47,5 +53,37 @@ public class PebbleBlock extends Block {
     @Override
     public VoxelShape getOutlineShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
         return SHAPE;
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        builder.add(VARIANT);
+    }
+
+    @Override
+    public @Nullable BlockState getPlacementState(ItemPlacementContext context) {
+        return getDefaultState().with(VARIANT, PebbleVariant.fromRandom(context.getWorld().getRandom()));
+    }
+
+    public enum PebbleVariant implements StringIdentifiable {
+        SMALL("small"),
+        FLAT("flat"),
+        LARGE("large");
+
+        private final String name;
+
+        PebbleVariant(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String asString() {
+            return name;
+        }
+
+        public static PebbleVariant fromRandom(Random random) {
+            PebbleVariant[] values = values();
+            return values[random.nextInt(values.length)];
+        }
     }
 }
