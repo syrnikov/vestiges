e98d4b4b849414f06f3ae82dbc6a96c07e89a0ec
Aaron Syrnikov <114866139+syrnikov@users.noreply.github.com>
2026-01-28 16:59:34 +0200

    Reduce forest rock rate in plains

--- src/main/java/com/noxtrnal/vestiges/worldgen/VestigesWorldgen.java
+++ src/main/java/com/noxtrnal/vestiges/worldgen/VestigesWorldgen.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.Identifier;
+import net.minecraft.world.biome.BiomeKeys;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.feature.PlacedFeature;
 
@@ -17,6 +18,10 @@ public final class VestigesWorldgen {
 	public static final RegistryKey<PlacedFeature> PEBBLE_PATCH = RegistryKey.of(
 			RegistryKeys.PLACED_FEATURE,
 			Identifier.of(Vestiges.MOD_ID, "pebble_patch")
+	);
+	public static final RegistryKey<PlacedFeature> RARE_FOREST_ROCK = RegistryKey.of(
+			RegistryKeys.PLACED_FEATURE,
+			Identifier.of(Vestiges.MOD_ID, "rare_forest_rock")
 	);
 
 	private VestigesWorldgen() {
@@ -32,6 +37,11 @@ public final class VestigesWorldgen {
 				BiomeSelectors.foundInOverworld(),
 				GenerationStep.Feature.VEGETAL_DECORATION,
 				PEBBLE_PATCH
+		);
+		BiomeModifications.addFeature(
+				BiomeSelectors.includeByKey(BiomeKeys.PLAINS),
+				GenerationStep.Feature.LOCAL_MODIFICATIONS,
+				RARE_FOREST_ROCK
 		);
 	}
 }
