add2114f4c94142cef545426bac4101e6461aba8
Aaron Syrnikov <114866139+syrnikov@users.noreply.github.com>
2026-01-28 09:42:07 +0200

    init

---
+++ src/client/java/com/noxtrnal/vestiges/VestigesClient.java
@@ -0,0 +1,10 @@
+package com.noxtrnal.vestiges;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VestigesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
